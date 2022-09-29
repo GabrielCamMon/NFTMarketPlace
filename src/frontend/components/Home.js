9cad129a58061284987dd98d6b8860b4a2fb9086
Gabriel Campos <gabrielcmdeveloper@gmail.com>
2022-09-28 21:31:56 -0300

    home fineshed

--- src/frontend/components/Home.js
+++ src/frontend/components/Home.js
@@ -1,52 +1,90 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import logo from "./logo.png";
+import { Col, Row, Card, Button } from "react-bootstrap";
+import { ethers } from "ethers";
 
 const Home = (marketplace, nft) => {
-  const [item, setItem] = useState([]);
+  const [items, setItems] = useState([]);
+  const [loading, setLoading] = useState(true);
   const loadMarketplaceItems = async () => {
     const itemCount = await marketplace.itemCount();
     let items = [];
     for (let i = 1; i <= itemCount; i++) {
       const item = await marketplace.items(i);
+
       if (!item.sold) {
         //get uir from nft contract
         const uri = await nft.tokenURI(item.tokenId);
+        // use uri to fetch the nft metadata stored on ipfs
+        const response = await fetch(uri);
+        const metadata = await response.json();
+        //get total price of item (item price + fee)
+        const totalPrice = await marketplace.getTotalPrice(item.itemId);
+        // Add item to items array
+        items.push({
+          totalPrice,
+          itemId: item.itemId,
+          seller: item.seller,
+          name: metadata.name,
+          description: metadata.description,
+          image: metadata.image,
+        });
       }
+      setItems(items);
+      setLoading(false);
     }
   };
+  const buyMarketItem = async (item) => {
+    await (
+      await marketplace.puchaseItem(item.itemId, { value: item.totalPrice })
+    ).wait();
+    loadMarketplaceItems();
+  };
+
+  useEffect(() => {
+    loadMarketplaceItems();
+  }, []);
+
+  if (loading)
+    return (
+      <main style={{ padding: "1rem 0" }}>
+        <h2>Loading...</h2>
+      </main>
+    );
   return (
-    <div>
-      <div className="container-fluid mt-5">
-        <div className="row">
-          <main role="main" className="col-lg-12 d-flex text-center">
-            <div className="content mx-auto mt-5">
-              <a
-                href="http://www.dappuniversity.com/bootcamp"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <img src={logo} className="App-logo" alt="logo" />
-              </a>
-              <h1 className="mt-5">Dapp University Starter Kit</h1>
-              <p>
-                Edit <code>src/frontend/components/App.js</code> and save to
-                reload.
-              </p>
-              <a
-                className="App-link"
-                href="http://www.dappuniversity.com/bootcamp"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                LEARN BLOCKCHAIN{" "}
-                <u>
-                  <b>NOW! </b>
-                </u>
-              </a>
-            </div>
-          </main>
+    <div className="flex justify-center">
+      {items.length > 0 ? (
+        <div className="px-5 container">
+          <Row xs={1} md={2} lg={4} className="g-4 py-5">
+            {items.map((item, idx) => (
+              <Col key={idx} className="overflow-hidden">
+                <Card>
+                  <Card.Img variant="top" src={item.image} />
+                  <Card.Body color="secondary">
+                    <Card.Title>{item.name}</Card.Title>
+                    <Card.Text>{item.description}</Card.Text>
+                  </Card.Body>
+                  <Card.Footer>
+                    <div className="d-grid">
+                      <Button
+                        onClick={() => buyMarketItem(item)}
+                        variant="primary"
+                        size="lg"
+                      >
+                        Buy for {ethers.utils.formatEther(item.totalPrice)} ETH
+                      </Button>
+                    </div>
+                  </Card.Footer>
+                </Card>
+              </Col>
+            ))}
+          </Row>
         </div>
-      </div>
+      ) : (
+        <main style={{ padding: "1rem 0" }}>
+          <h2>No listed assets</h2>
+        </main>
+      )}
     </div>
   );
 };
